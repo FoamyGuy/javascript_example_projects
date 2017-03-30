53a3332214d2722e13b7b70e02f897dd085a4cea
android_ninja <foamyguy@gmail.com>
2017-03-29 23:02:22 -0500

    change the button text when it's clicked

--- hide_and_show/js/script.js
+++ hide_and_show/js/script.js
@@ -2,5 +2,11 @@ var $collapse_btn = document.getElementById("collapse-btn");
 var $collapsable_div = document.getElementById("collapsable-div");
 
 $collapse_btn.addEventListener("click", function(){
+	if($collapse_btn.innerHTML == "Hide"){
+		$collapse_btn.innerHTML = "Expand"
+	}else{
+		$collapse_btn.innerHTML = "Hide";
+	}
+	
 	$collapsable_div.classList.toggle("hidden");
 });
